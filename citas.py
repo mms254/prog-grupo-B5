e81b4bf6434519c48215494fd00c3e153dc512aa
rrricardoua23 <rlm78@alu.ua.es>
2025-04-09 18:56:40 +0200

    Update citas.py

--- citas.py
+++ citas.py
@@ -1,11 +1,14 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
+
 class Cita(ABC):
-    def __init__(self, id_cita, paciente, medico, fecha, hora, estado='pendiente', atendido=False):
+    def __init__(self, id_cita, paciente, medico, fecha_hora: str, motivo: str, estado='pendiente', atendido=False):
         self.id_cita = id_cita
         self.paciente = paciente
         self.medico = medico
-        self.fecha = fecha
-        self.hora = hora
+        self.fecha_hora = fecha_hora
+        self.motivo = motivo
+        self.fecha_hora_dt = datetime.strptime(fecha_hora, "%Y-%m-%d %H:%M")
         self.estado = estado
         self.atendido = atendido
     @abstractmethod
@@ -18,6 +21,10 @@ class Cita(ABC):
         self.atendido = True
         self.estado = 'completado'
         return self.atendido
+
+    def se_solapa(self, otra_cita) -> bool:
+        delta = abs((self.fecha_hora_dt - otra_cita.fecha_hora_dt).total_seconds())
+        return delta < 1800
 
     def to_dict(self):
         return {
