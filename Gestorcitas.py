48e41d4a8a30172a664eded00fe2e49e619efe52
María Mestre Sánchez <mms254@alu.ua.es>
2025-04-10 19:22:36 +0200

    Archivos definitivos

---
+++ Gestorcitas.py
@@ -0,0 +1,164 @@
+from abc import ABC, abstractmethod
+from typing import List
+from citas import Cita
+from paciente import Paciente
+
+class CitaPresencial(Cita):
+
+    """ Clase que hereada directamente de la clase abstracta Cita dentro de
+    nuestra base de datos """
+
+    def __init__(self, id_cita: int, paciente: Paciente, medico : str, fecha : str, hora: str, centro: str):
+
+        """ Parámetros:
+            -----------
+             - Id_cita -> Id unico que identifica cada cita pedida
+             - Paciente -> El paciente que pide o recibe esta cita
+             - medico -> Medico asociado a dicha cita
+             - fecha -> Fecha de la cita
+             - hora -> Hora de la cita
+             - centro -> Centro de la cita
+        """
+
+        super().__init__(id_cita, paciente, medico,fecha,hora)
+        self.centro = centro
+
+    def cancelar_cita(self) -> str:
+        self.estado = 'Cancelada'
+        return f'La cita presencial {self.id_cita} el dia {self.fecha} en el centro{self.centro} y ha sido cancelada'
+
+    def ser_atendido(self):
+        self.atendido = True
+        return f'El paciente {self.paciente.nombre} esta siendo atendido'
+
+
+class CitaTelefonica(Cita):
+
+    """ Clase que hereda directamente de la clase abstracta Cita dentro de la base de datos creada"""
+
+    def __init__(self, id_cita: int, paciente: Paciente, medico: str, fecha: str, hora: str, telefono_contacto: str):
+
+        """ Parametros:
+            -----------
+             - Id_cita -> Id unico que identifica cada cita pedida
+             - Paciente -> El paciente que pide o recibe esta cita
+             - medico -> Medico asociado a dicha cita
+             - fecha -> Fecha de la cita
+             - hora -> Hora de la cita
+             - telefono_contacto -> Telefono de contacto para la llamada
+
+        """
+        super().__init__(id_cita, paciente, medico, fecha, hora)
+        self.telefono_contacto = telefono_contacto
+
+    def cancelar_cita(self) -> str:
+        self.estado = 'Cancelada'
+        self.atendido = False
+        return f'La cita telefónica {self.id_cita} ha sido cancelada'
+
+    def ser_atendido(self):
+        self.atendido = True
+        return f'El paciente {self.paciente.nombre} esta siendo atendido por teléfono al {self.telefono_contacto}'
+
+
+class CitaUrgencias(Cita):
+
+    """ Esta nueva clase vuelve a heredar de la clase base Cita (creada en la base de datos)
+    pero en este caso se representan las citas de urgencias dentro del hospital """
+
+    def __init__(self, id_cita: int, paciente: Paciente, medico: str, fecha: str, hora: str, nivel_prioridad: str):
+
+        """ Parametros:
+            -----------
+             - Id_cita -> Id unico que identifica cada cita pedida
+             - Paciente -> El paciente que pide o recibe esta cita
+             - medico -> Medico asociado a dicha cita
+             - fecha -> Fecha de la cita
+             - hora -> Hora de la cita
+             - nivel_prioridad -> Nivel de la urgencia
+             """
+        super().__init__(id_cita, paciente, medico, fecha, hora)
+        self.nivel_prioridad = nivel_prioridad
+
+    def cancelar_cita(self) -> str:
+        self.estado = 'Cancelada'
+        self.atendido = False
+        return f'La cita de urgencia ha sido cancelada'
+
+    def ser_atendido(self):
+        self.atendido = True
+        return f'El paciente {self.paciente.nombre} esta siendo atendido de urgencia debido a que su prioridad es {self.nivel_prioridad}'
+
+
+class GestorCitas:
+
+    """
+        Gestiona la creación, almacenamiento y control de citas médicas.
+        """
+
+    def __init__(self):
+        """
+        Inicializa un nuevo gestor de citas.
+
+        Atributos:
+        -----------
+        lista_citas : List[Cita]
+            Lista que almacena las citas médicas creadas. Cada objeto de cita debe contener:
+                - id_cita: Id único que identifica cada cita pedida.
+                - paciente: El paciente que pide o recibe la cita.
+                - medico: Médico asociado a dicha cita.
+                - fecha: Fecha de la cita.
+                - hora: Hora de la cita.
+                DEPENDIENDO DE LA CITA:
+                - centro -> Centro de la cita
+                - telefono_contacto -> Telefono de contacto para la llamada
+                - nivel_prioridad: Nivel de la urgencia (aplicable a citas de urgencias).
+        """
+
+        self.lista_citas: List[Cita] = []
+
+    def añadir_cita(self, cita:Cita) -> None:
+
+        """ Añade una nueva cita"""
+
+        self.lista_citas.append(cita)
+
+    def cancelar_cita(self, id_cita : int) -> str:
+
+        """ Cancela una cita dependiendo de su ID """
+
+        for cita in self.lista_citas:
+            if cita.id_cita == id_cita:
+                return cita.cancelar_cita()
+        return 'Cita no encontrada'
+
+    def atender_cita(self, id_cita: int) -> str:
+
+        """ Marca que una cita ha sido atentdida dependiendo de su id"""
+
+        for cita in self.lista_citas:
+            if cita.id_cita == id_cita:
+                return cita.ser_atendido()
+        return 'Cita no encontrada'
+
+    def mostrar_citas(self) -> None:
+
+        """ Muestra todas las citas """
+
+        for cita in self.lista_citas:
+            if cita.atendido:
+                estado = "Atendido"
+            else:
+                estado = "Pendiente"
+
+            print(f'{cita.id_cita} - {cita.__class__.__name__} - Paciente: {cita.paciente.nombre} - Médico: {cita.medico} - {estado}')
+
+    def __iadd__(self, cita:Cita):
+
+        """
+            Sobrecarga del operador += para añdir una cita al gestor.
+            Permite usar : gestor += cita
+        """
+
+        self.añadir_cita(cita)
+        return self
---
+++ asignacion.py
@@ -0,0 +1,90 @@
+import random
+from typing import List
+from medico import Medico
+from paciente import Paciente
+
+class Asignaciones:
+
+    """
+    Clase encargada de gestionar la asignación de médicos a paciente
+
+    """
+
+    def __init__(self, medicos : List[Medico], pacientes: List[Paciente]):
+
+        """
+            Parámetros:
+            -----------
+
+            Inicializa el gestor con una lista de médicos disponibles
+
+            medicos: Lista de instancias de la clase Médico
+            pacientes: Lista de instancias de la clase Paciente
+
+        """
+        self.medicos = medicos
+        self.pacientes = pacientes
+        self.asignaciones = {}
+
+    def cambiar_disponibilidad(self, medico_id :str, disponible :bool) -> None:
+
+        """
+                Cambia la disponibilidad de un médico dado su ID.
+
+                medico_id: ID del médico al que se desea cambiar la disponibilidad.
+                disponible: True si el médico debe estar disponible, False si no.
+        """
+        for medico in self.medicos:
+            if medico.id == medico_id:
+                medico.disponibilidad = disponible
+                print(f"La disponibilidad del médico {medico.nombre} ha sido actualizada.")
+                return
+
+            print("No se encontró un médico con ese ID.")
+
+    def medicos_disponibles(self) -> List[Medico]:
+
+        """
+            Devuelve una lista con los médicos disponibles
+
+        """
+        disponibles = []
+        for medico in self.medicos:
+            if medico.disponibilidad:
+                disponibles.append(medico)
+        return disponibles
+
+    def asignar(self, paciente : Paciente) -> bool:
+
+        """
+            Asigna aleatoriamente un médico disponible a un paciente.
+            Retorna True si se realizó la asignación con éxito
+
+        """
+
+        disponibles = self.medicos_disponibles()
+        if not disponibles:
+            print(f"No hay médicos disponibles para el paciente {paciente.nombre}")
+            return False
+
+        medico = random.choice(disponibles)
+        self.asignaciones[paciente.id] = medico.id
+
+        print(f"Paciente {paciente.nombre} ha sido asignado al médico {medico.nombre}")
+        return True
+
+
+    def mostrar_asignaciones(self) -> None:
+
+        """
+            Muestra todas las asignaciones realizadas
+
+        """
+
+        if not self.asignaciones:
+            print('No hay asignaciones para el paciente')
+            return
+
+        print("Lista de asignaciones:")
+        for pac_id , med_id in self.asignaciones.items():
+            print(f"Paciente ID :{pac_id} -> Médico Id {med_id}")
