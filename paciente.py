3a5de76498c9e277a6c81eeb7fec020ed0db3347
María Mestre Sánchez <mms254@alu.ua.es>
2025-04-10 18:46:51 +0200

    Archivos definitivos

--- paciente.py
+++
@@ -1,7 +0,0 @@
-from persona import Persona
-
-class Paciente(Persona):
-    def __init__(self, id, nombre, apellido, edad, genero, estado, medico_asignado=None):
-        super().__init__(id, nombre, apellido, edad, genero)
-        self.estado = estado
-        self.medico_asignado = medico_asignado
